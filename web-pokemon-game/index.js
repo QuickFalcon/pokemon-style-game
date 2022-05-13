e4501e0a04e085e1ed5fd0b7aa8c7da33b1003c8
QuickFalcon <sosa3500@hotmail.com>
2022-05-13 09:11:24 -0500

    Center the player

--- web-pokemon-game/index.js
+++ web-pokemon-game/index.js
@@ -11,8 +11,10 @@ ctx.fillRect(0, 0 , canvas.width, canvas.height)
 const image = new Image()
 image.src = './img/Pellet Town.png'
 
-console.log(image)
+const playerImage = new Image()
+playerImage.src = './img/playerDown.png'
 
 image.onload = () => {
     ctx.drawImage(image, -750, -550)
+    ctx.drawImage(playerImage, canvas.width/2, canvas.height/2)
 }
