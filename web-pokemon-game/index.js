419c1336053c3fb320eac8c1e37c3c7ff7fce987
QuickFalcon <sosa3500@hotmail.com>
2022-05-13 08:35:58 -0500

    Adding image to canvas

--- web-pokemon-game/index.js
+++ web-pokemon-game/index.js
@@ -7,3 +7,13 @@ canvas.height = 576
 
 ctx.fillStyle = 'white'
 ctx.fillRect(0, 0 , canvas.width, canvas.height)
+
+const image = new Image()
+image.src = './img/Pellet Town.png'
+
+console.log(image)
+
+image.onload = () => {
+    console.log('Loaded!!!!!')
+    ctx.drawImage(image, 0, 0)
+}
